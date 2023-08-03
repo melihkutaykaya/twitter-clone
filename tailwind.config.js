7bb46d985535e55090bb38ec2161242a6e5b1ab9
Melih Kutay Kaya <melihkaya33@gmail.com>
2023-08-04 01:04:23 +0300

    design finished

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    color: {
+      'twitter' : '#1DA1F2',
+      'brdr' : '#657786'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
